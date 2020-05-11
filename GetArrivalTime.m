d94c52791a93eaa235289ae93954fb18de6cbeb7
Sobir Ali <alisobir@gmail.com>
2020-05-11 21:42:21 +0530

    Delete GetArrivalTime.m

--- GetArrivalTime.m
+++
@@ -1,9 +0,0 @@
-function [request_time] = GetArrivalTime(lambda,t)
-
-dt = - log ( rand ( 1, 1 ) ) / lambda; % inter_arrival times
-t = t + dt;  % request arriving TIME INSTANT
-request_time = t;
-
-
-
-
